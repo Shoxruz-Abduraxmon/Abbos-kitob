9fa2012adcbe4d554a5ec78fcb0a4f55795d9c7f
Manmohan Singh <manmohan.s@devtrust.biz>
2023-04-11 11:55:07 +0530

    Added docker file for project

--- index.js
+++ index.js
@@ -1,80 +1,73 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
 // const cookieParser = require('cookie-parser');
 // const session = require('express-session');
-const flash = require('connect-flash');
-const { get } = require('http');
+const flash = require("connect-flash");
+const { get } = require("http");
 const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
+const sessions = require("express-session");
 dotenv.config();
 
 const app = express();
 
 const oneDay = 1000 * 60 * 60 * 24;
-app.use(sessions({
+app.use(
+  sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767asasa",
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: oneDay },
-    resave: false 
-}));
+    resave: false,
+  })
+);
 app.use(cookieParser());
 
-
 //Routes
-const homeRouter = require('./routes/home');
-const mundarijaRouter = require('./routes/mundarija');
-const kitobRouter = require('./routes/kitob');
-const loginRouter = require('./routes/login');
-const registratsiyaRouter = require('./routes/registratsiya');
-const bookget = require('./routes/bookget');
-const { error } = require('console');
-
-
-
-
-
+const homeRouter = require("./routes/home");
+const mundarijaRouter = require("./routes/mundarija");
+const kitobRouter = require("./routes/kitob");
+const loginRouter = require("./routes/login");
+const registratsiyaRouter = require("./routes/registratsiya");
+const bookget = require("./routes/bookget");
+const { error } = require("console");
 
 const hbs = exphbs.create({
-    defaultLayout : 'main',
-    extname : 'hbs'
+  defaultLayout: "main",
+  extname: "hbs",
 });
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', 'views');
-app.use(express.urlencoded({extended: true}));
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 app.use(homeRouter);
 app.use(mundarijaRouter);
 app.use(kitobRouter);
-app.use(loginRouter); 
+app.use(loginRouter);
 app.use(registratsiyaRouter);
 app.use(express.json());
 app.use(flash());
 
+const startApp = async () => {
+  try {
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+    console.log("Connected to MongoDB");
 
-const startApp = () => {
-try{
-  mongoose.set('strictQuery', false);  
-  mongoose.connect(process.env.MONGO_URI, 
-    {useNewUrlParser: true},
-    () =>
-    console.log("Connected to MongoDB")
-  );
-  const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 8080;
 
     app.listen(PORT, () => {
       console.log(`open server ${PORT}`);
-});
-}catch (error) {
-  console.log(error)
-}
-}
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 //   } catch (error) {
 //     console.log(error);
 //   }
